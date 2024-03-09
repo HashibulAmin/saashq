c24bed8811cd3c85d039d610f8d33beb74514a44
SaasHQ Dev <saashqdev@gmail>
2024-03-09 02:53:31 +0000

    perf: (pos) show items sum amount on market theme

--- pos/modules/checkout/components/cart/cart.market.tsx
+++ pos/modules/checkout/components/cart/cart.market.tsx
@@ -12,12 +12,12 @@ const Cart = () => {
       <div className="mb-1 flex rounded border-transparent bg-primary px-3.5 py-3 text-sm leading-4 text-white">
         <div className="flex w-5/12">
           <small className="w-1/12">#</small>
-          <small className="w-11/12">Барааны нэр</small>
+          <small className="w-11/12">Product name</small>
         </div>
-        <small className="w-3/12">Тоо ширхэг</small>
+        <small className="w-3/12">Quantity</small>
         <small className="flex w-4/12">
-          <span className="w-6/12">Бар код</span>
-          <span className="w-5/12">Үнэ</span>
+          <span className="w-5/12">Price</span>
+          <span className="w-6/12">Total price</span>          
         </small>
       </div>
       <ScrollArea>
