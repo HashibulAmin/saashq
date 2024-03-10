d2b88caacfe95631b36be7320025982a34bb9897
SaasHQ Dev <saashqdev@gmail>
2024-03-10 20:06:05 +0000

    update dashboard api

--- packages/plugin-dashboard-ui/src/index.js
+++
@@ -1,6 +0,0 @@
-import App from './App';
-import '@saashq/ui/src/styles/global-styles';
-import 'saashq-icon/css/saashq.min.css';
-import '@saashq/ui/src/styles/style.min.css';
-
-export default App;
