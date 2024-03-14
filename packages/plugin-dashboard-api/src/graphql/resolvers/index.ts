a77760a7609bf042aeb3a7a578312e7b752084a7
SaasHQ Dev <saashqdev@gmail>
2024-03-14 18:11:20 +0000

    update dashboard-api

---
+++ packages/plugin-dashboard-api/src/graphql/resolvers/index.ts
@@ -0,0 +1,18 @@
+import dashboardMutations from './dashboardMutaions';
+import dashboardQueries from './dashboardQueries';
+import customScalars from '@saashq/api-utils/src/customScalars';
+import Dashboard from './dashboard';
+
+const resolvers: any = {
+  ...customScalars,
+  Dashboard,
+
+  Mutation: {
+    ...dashboardMutations,
+  },
+  Query: {
+    ...dashboardQueries,
+  },
+};
+
+export default resolvers;
