2aac483617b3ab04d4e41c49407a97b1fadc3f04
SaasHQ Dev <saashqdev@gmail>
2024-03-15 21:58:05 +0000

    fix syntax

--- packages/plugin-inbox-ui/src/bookings/constants.tsx
+++ packages/plugin-inbox-ui/src/bookings/constants.tsx
@@ -29,7 +29,7 @@ export const EMPTY_CONTENT_BOOKINGS = {
     {
       title: __('Prepare Product Properties'),
       description: __(
-        'This widget is based on your saashq Products and Services. Depending on your products, you may need to create custom Properties first. For example, you can display additional information such as Amenities, Services, etc. in the product detail page or as user filters.',
+        'This widget is based on your SaasHQ Products and Services. Depending on your products, you may need to create custom Properties first. For example, you can display additional information such as Amenities, Services, etc. in the product detail page or as user filters.',
       ),
       url: '/settings/properties?type=products:product',
       urlText: 'Create Custom Properties',
