c0eaab97bde53370868fcb7acb29139408ec0f2d
SaasHQ Dev <saashqdev@gmail>
2024-03-15 19:13:59 +0000

    Do not try to find organization in case of instrospection queries

--- packages/gateway/src/proxy/targets.ts
+++ packages/gateway/src/proxy/targets.ts
@@ -1,4 +1,7 @@
-import { getService, getServices } from '@saashq/api-utils/src/serviceDiscovery';
+import {
+  getService,
+  getServices,
+} from '@saashq/api-utils/src/serviceDiscovery';
 import retry from '../util/retry';
 import fetch from 'node-fetch';
 import * as dotenv from 'dotenv';
@@ -47,19 +50,30 @@ async function ensureGraphqlEndpointIsUp({
 
   const endponit = `${address}/graphql`;
 
+  /*
+    query: 'query SubgraphIntrospectQuery {\n' +
+      '    # eslint-disable-next-line\n' +
+      '    _service {\n' +
+      '        sdl\n' +
+      '    }\n' +
+      '}',
+  */
+
   const res = await fetch(endponit, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
+      variables: null,
       query: `
-          query _SaasHQGatewaySubgraphCheck_ {
+          query SubgraphIntrospectQuery {
             _service {
               sdl
             }
           }
           `,
+      operationName: 'SubgraphIntrospectQuery',
     }),
   });
   if (res.status === 200) {
