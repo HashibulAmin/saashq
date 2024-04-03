3c368c4d725c30057f44f73dd10789e998b013a7
SaasHQ Dev <saashqdev@gmail>
2024-04-03 20:03:47 +0000

    update widgets locale

--- shq-web/app/layout.tsx
+++ shq-web/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="cs">
+    <html lang="en">
       <head>
         <Script
           strategy="beforeInteractive"
