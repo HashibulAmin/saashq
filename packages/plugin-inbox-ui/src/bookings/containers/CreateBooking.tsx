cf2298601642a459e33044f6320e1782c867b694
SaasHQ Dev <saashqdev@gmail>
2024-04-04 22:56:31 +0000

    fix inbox-ui

--- packages/plugin-inbox-ui/src/bookings/containers/CreateBooking.tsx
+++ packages/plugin-inbox-ui/src/bookings/containers/CreateBooking.tsx
@@ -2,7 +2,7 @@ import * as compose from 'lodash.flowright';
 
 import {
   AddBookingIntegrationMutationResponse,
-  AddBookingIntegrationMutationVariables
+  AddBookingIntegrationMutationVariables,
 } from '../types';
 
 import { Alert } from '@saashq/ui/src/utils';
@@ -54,7 +54,7 @@ class CreateBookingContainer extends React.Component<FinalProps, State> {
 
     this.state = {
       loading: false,
-      isReadyToSaveForm: false
+      isReadyToSaveForm: false,
     };
   }
 
@@ -64,7 +64,7 @@ class CreateBookingContainer extends React.Component<FinalProps, State> {
       history,
       emailTemplatesQuery,
       fieldsQuery,
-      configsQuery
+      configsQuery,
     } = this.props;
 
     const afterFormDbSave = (id: string) => {
@@ -74,15 +74,15 @@ class CreateBookingContainer extends React.Component<FinalProps, State> {
         addIntegrationMutation({
           variables: {
             ...this.state.doc,
-            formId: id
-          }
+            formId: id,
+          },
         })
           .then(() => {
             Alert.success('You successfully added a booking');
             history.push('/bookings');
           })
 
-          .catch(error => {
+          .catch((error) => {
             Alert.error(error.message);
           })
           .finally(() => {
@@ -91,7 +91,7 @@ class CreateBookingContainer extends React.Component<FinalProps, State> {
       }
     };
 
-    const save = doc => {
+    const save = (doc) => {
       this.setState({ loading: false, isReadyToSaveForm: true, doc });
     };
 
@@ -105,7 +105,7 @@ class CreateBookingContainer extends React.Component<FinalProps, State> {
         ? emailTemplatesQuery.emailTemplates || []
         : [],
       productFields: fieldsQuery.fields || [],
-      configs: configsQuery.configs || []
+      configs: configsQuery.configs || [],
     };
     return <Booking {...updatedProps} />;
   }
@@ -114,23 +114,23 @@ class CreateBookingContainer extends React.Component<FinalProps, State> {
 export default compose(
   graphql(gql(queries.templateTotalCount), {
     name: 'emailTemplatesTotalCountQuery',
-    skip: !isEnabled('engages') ? true : false
+    skip: !isEnabled('engages') ? true : false,
   }),
   graphql<FinalProps>(gql(queries.emailTemplates), {
     name: 'emailTemplatesQuery',
     options: ({ emailTemplatesTotalCountQuery }) => ({
       variables: {
-        perPage: emailTemplatesTotalCountQuery.emailTemplatesTotalCount
-      }
+        perPage: emailTemplatesTotalCountQuery.emailTemplatesTotalCount,
+      },
     }),
-    skip: !isEnabled('engages') ? true : false
+    skip: !isEnabled('engages') ? true : false,
   }),
   graphql<
     {},
     AddBookingIntegrationMutationResponse,
     AddBookingIntegrationMutationVariables
   >(gql(mutations.integrationsCreateBooking), {
-    name: 'addIntegrationMutation'
+    name: 'addIntegrationMutation',
   }),
   graphql<{}, FieldsQueryResponse, { contentType: string }>(
     gql(queries.fields),
@@ -138,12 +138,12 @@ export default compose(
       name: 'fieldsQuery',
       options: () => ({
         variables: {
-          contentType: FIELDS_GROUPS_CONTENT_TYPES.PRODUCT
-        }
-      })
-    }
+          contentType: FIELDS_GROUPS_CONTENT_TYPES.PRODUCT,
+        },
+      }),
+    },
   ),
   graphql<{}, ConfigsQueryResponse>(gql(settingsQueries.configs), {
-    name: 'configsQuery'
-  })
-)(withRouter<FinalProps>(CreateBookingContainer));
+    name: 'configsQuery',
+  }),
+)(withRouter<FinalProps, any>(CreateBookingContainer));
